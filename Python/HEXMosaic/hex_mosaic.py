e8f969db373d6af307d7d5141b7c6770d03969bf
kmwtr <0x550x45@gmail.com>
2021-02-11 22:29:46 +0900

    first commit

---
+++ Python/HEXMosaic/hex_mosaic.py
@@ -0,0 +1,18 @@
+#! python3
+import os
+import logging as log
+
+# Debug Setting
+log.basicConfig(level=log.DEBUG, format='%(asctime)s | %(levelname)s | %(message)s')
+
+# -------------------------------------------------
+
+def hex_mosaic():
+    log.debug('-> hex_mosaic()')
+
+
+# -------------------------------------------------
+
+if __name__ is '__main__':
+    hex_mosaic()
+    #os.system('PAUSE')
